ae686a1b53c4364cc33f65552d4d9433f66891f1
mnunezjimenez <marconunezj@outlook.com>
2023-04-30 11:25:04 -0600

    Corrected structure
    Joined a single API file

--- hackathon-front/MovilityApp/MovilityApp/ViewModel/API.swift
+++ hackathon-front/MovilityApp/MovilityApp/ViewModel/API.swift
@@ -1,11 +1,25 @@
 //
-//  Register.swift
+//  API.swift
 //  MovilityApp
 //
 //  Created by Marco Núñez on 30/04/23.
 //
 
 import Foundation
+
+func isLoginSuccessful(username: String, password: String, completion:@escaping ([String]) -> ()) {
+    guard let url = URL(string: "http://122.8.178.68:3000/login?user_name="+username+"&password="+password) else {
+            print("Invalid url...")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let result = try! JSONDecoder().decode([String].self, from: data!)
+            print(result)
+            DispatchQueue.main.async {
+                completion(result)
+        }
+    }.resume()
+}
 
 func isRegisterSuccessful(username: String, password: String, completion:@escaping ([String]) -> ()) {
     guard let url = URL(string: "http://122.8.178.68:3000/add_user?user_name="+username+"&password="+password) else {
---
+++ hackathon-front/MovilityApp/MovilityApp/Views/TerminalsView.swift
@@ -0,0 +1,22 @@
+//
+//  TerminalsView.swift
+//  MovilityApp
+//
+//  Created by Marco Núñez on 30/04/23.
+//
+
+import SwiftUI
+
+struct TerminalsView: View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+struct TerminalsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TerminalsView()
+    }
+}
